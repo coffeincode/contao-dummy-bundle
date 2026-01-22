b96bd17a9639547e0e33c1d3515e4861aabdd400
Marina Diezler <md@diezler.net>
2026-01-22 15:09:21 +0100

    Basis-Struktur zum Test, ob Repo erreichbar ist

---
+++ src/ContaoDummyBundle.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Coffeincode\ContaoDummyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class ContaoDummyBundle extends AbstractBundle {
+}
---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Coffeincode\ContaoDummyBundle\ContaoManager;
+
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Coffeincode\ContaoDummyeBundle\ContaoDummyBundle;
+
+class Plugin implements BundlePluginInterface {
+	public function getBundles(ParserInterface $parser) : array{
+		return [
+			BundleConfig::create(ContaoDummyBundle::class)->setLoadAfter([ContaoCoreBundle::class]),
+		];
+	}
+
+}
